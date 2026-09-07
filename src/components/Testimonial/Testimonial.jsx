a73450758ddb8f9972ab4542702c9339a0a1cf8b
oscarperezGR <oscarperezlds5@gmail.com>
2026-09-07 15:29:29 +0200

    feat: add Header component and update Testimonial component

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -2,17 +2,20 @@ import './Testimonial.css';
 
 function Testimonial({ name, country, role, company, quote, image }) {
   return (
-    
-    <article className="testimonialCard">
-      <img className="testimonialPhoto" src={image} alt={`Foto de ${name}`} />
-      <div className="testimonialContent">
-      <h2 className="testimonialHeading">{name} en {country}</h2>
-      <p className="testimonialCompany">{company}</p>
-      <blockquote className="testimonialQuote">
-        <p>{quote}</p>
-      </blockquote>
-      </div>
-    </article>
+    <section>
+      <h1>Testimonios</h1>
+      <h2>de personas como reales como tu.</h2>
+      <article className="testimonialCard">
+        <img className="testimonialPhoto" src={image} alt={`Foto de ${name}`} />
+        <div className="testimonialContent">
+          <h2 className="testimonialHeading">{name} en {country}</h2>
+          <p className="testimonialCompany">{company}</p>
+          <blockquote className="testimonialQuote">
+            <p>{quote}</p>
+          </blockquote>
+        </div>
+      </article>
+    </section>
   );
 }
 
