44494065d1470e93374abc7f8fc0a03357b89d71
oscarperezGR <oscarperezlds5@gmail.com>
2026-09-07 15:20:55 +0200

    fix: new line

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -7,7 +7,7 @@ function Testimonial({ name, country, role, company, quote, image }) {
       <img className="testimonialPhoto" src={image} alt={`Foto de ${name}`} />
       <div className="testimonialContent">
       <h2 className="testimonialHeading">{name} en {country}</h2>
-      <p className="testimonialPosition">{role} en <strong>{company}</strong></p>
+      <p className="testimonialCompany">{company}</p>
       <blockquote className="testimonialQuote">
         <p>{quote}</p>
       </blockquote>
