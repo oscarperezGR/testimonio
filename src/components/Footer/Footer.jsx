47752c4e7c71452c2a60080bea361c32847e82dd
oscarperezGR <oscarperezlds5@gmail.com>
2026-09-07 15:40:10 +0200

    feature: add Footer component and update Testimonial component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="mainFooter">
+      <p className="mainFooterText">Todos los derechos reservados</p>
+    </footer>
+  );
+}
+
+export default Footer;
